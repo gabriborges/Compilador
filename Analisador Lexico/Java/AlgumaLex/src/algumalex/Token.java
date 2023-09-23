0a9f40500b974098a976af189853134bd60a0033
Gabriel Borges <94412072+gabriborges@users.noreply.github.com>
2023-09-22 22:05:21 -0300

    Add files via upload

---
+++ Analisador Lexico/Java/AlgumaLex/src/algumalex/Token.java
@@ -0,0 +1,16 @@
+
+package algumalex;
+
+public class Token {
+    public TipoToken nome;
+    public String lexema;
+
+    public Token(TipoToken nome, String lexema) {
+        this.nome = nome;
+        this.lexema = lexema;
+    }
+    @Override
+    public String toString() {
+        return "<"+nome+","+lexema+">";
+    }
+}
